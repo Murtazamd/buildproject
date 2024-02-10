69c1b25b9be325ce16397012c1f72e53fdaf746b
Murtazamd <122963039+Murtazamd@users.noreply.github.com>
2024-02-10 19:56:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/webapp1/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.webapp1.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Welcome To Technical Cloud Knowledge."
+			+
+			"\n"
+			+
+			"MD Name Sanjay Dahiya (v0.2)"
+			+
+			"\n";
+	}
+
+}
